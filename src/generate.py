1339bdf142ba7b4445b8d073f16305e0a49a3f68
John Naylor <jonaylor89@gmail.com>
2020-11-22 18:42:59 -0500

    no c++

--- src/generate.py
+++ src/generate.py
@@ -1,29 +1,30 @@
-
 import sys
 import networkx as nx
 
+
 def generate(filename, graph, nodes, edges):
 
-    with open(filename,"w") as file:
+    with open(filename, "w") as file:
 
-        file.write(str(nodes)+" "+str(edges)+"\n")
+        file.write(str(nodes) + " " + str(edges) + "\n")
 
-        for i,j in graph.edges():
-            file.write(str(i+1) +" " + str(j+1)+"\n")
+        for i, j in graph.edges():
+            file.write(str(i + 1) + " " + str(j + 1) + "\n")
 
         colors = " ".join(["0" for i in range(0, nodes)])
 
         file.write(colors)
 
+
 def main():
     if len(sys.argv) < 3:
         print("Usage: \n\tpython3 generate.py nodes edges")
-        return 
+        return
 
     numNodes = int(sys.argv[1])
     numEdges = int(sys.argv[2])
-    G = nx.gnm_random_graph(numNodes,numEdges)
-    
+    G = nx.gnm_random_graph(numNodes, numEdges)
+
     generate("inputs/graph.txt", G, numNodes, numEdges)
 
 
--- src/main.py
+++ src/main.py
@@ -1,19 +1,20 @@
-
 from collections import defaultdict
 
-def color_nodes(graph, color_map):
 
+def color_graph(graph, color_map):
 
-    # Consider nodes in descending degree
     for node in sorted(graph, key=lambda x: len(graph[x]), reverse=True):
-        if color_map[node-1] == 0:
-            neighbor_colors = set(color_map.get(neigh-1) for neigh in graph[node])
+        if color_map[node - 1] == 0:
+            neighbor_colors = set(color_map.get(neigh - 1) for neigh in graph[node])
 
-            color_map[node-1] = next(
-                color for color in range(1, len(graph)+1) if color not in neighbor_colors
+            color_map[node - 1] = next(
+                color
+                for color in range(1, len(graph) + 1)
+                if color not in neighbor_colors
             )
 
     return color_map
+
 
 def main():
 
@@ -28,17 +29,14 @@ def main():
 
         graph[_to].append(_from)
         graph[_from].append(_to)
-    
+
     color_map = {}
     for i, color in enumerate(colors):
         color_map[i] = color
 
-    # print(graph)
-    # print(color_map)
+    solution = color_graph(graph, color_map)
 
-    solution = color_nodes(graph, color_map)
-
-    original_colors = len(set(colors)) if 0 not in set(colors) else len(set(colors))-1
+    original_colors = len(set(colors)) if 0 not in set(colors) else len(set(colors)) - 1
     new_colors = len(set(solution.values()))
     color_diff = new_colors - original_colors
 
@@ -48,6 +46,7 @@ def main():
         print(v, end=" ")
 
     print()
+
 
 if __name__ == "__main__":
     main()
